25d88efed86e2c27bf8bdeba8ef84de4853a555f
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-06-14 17:28:25 -0500

    Added a DisplayResults() , a FightLog struct to contain the fight result history, and began FightSim() functionality
    FightSim() will use Rounds to keep track of the current roster iteration of fights , this will be used to preventing repeat fights

--- Exercises/Exercise2/Assets/Scripts/Exercise2_MonsterBrawl/MonsterBrawl.cs
+++ Exercises/Exercise2/Assets/Scripts/Exercise2_MonsterBrawl/MonsterBrawl.cs
@@ -40,9 +40,20 @@ public class MonsterBrawl : MonoBehaviour
         public int HealthPoints;
         public int SpeedStat;
     }
-
     // Will hold the roster
     [SerializeField] private Monster[] monsters;
+
+    // Used to keep track of who wins each fight as well as fight duration (in turns), match will be displayed to console on conclusion
+    [System.Serializable]
+    public struct FightLog
+    {
+        public string FighterA;
+        public string FighterB;
+        public string Winner;
+        public int WinnerFinalHP;
+        public int TotalTurns;
+    }
+    [SerializeField] private FightLog fightLog;
 
     void Start()
     {
@@ -67,8 +78,50 @@ public class MonsterBrawl : MonoBehaviour
         }
     }
 
+    /* Simulate every unique 1v1 fight
+     *      1.Every monster should fight every other monster exactly once.
+     *          Goblin vs Orc and Orc vs Goblin are the same fight � only one should occur.
+     *      2. A monster should never fight itself.
+     *      3. In each fight, both monsters attack simultaneously each turn.
+     *      4. A monster only attacks on turns that are a multiple of its speed.
+     * E.g.a monster with speed 2 attacks on turns 2, 4, 6, etc.A monster with speed 3 attacks on turns 3, 6, 9, etc.
+     *      5. The fight ends when one or both monsters reach 0 HP or below.
+     *      6. Print each fight result in this exact format:
+     *          Goblin vs Orc | Winner: Orc | Turns: 12 | Remaining HP: 24
+     *      7. If both monsters die on the same turn, print:
+     *          Goblin vs Orc | Draw | Turns: 8
+     *  Instructions:
+     *      Attach the script to any active GameObject in your Unity scene.
+     * Observe the results in the Console after hitting the Play button.
+     */
+
     void FightSim(Monster[] monsters)
     {
+        int roundCount = 0; // starts at round 0
+        int turnCount;
 
+        for (int i = roundCount; i < monsters.Length; i++)
+        {
+            fightLog.FighterA = monsters[i].Name;
+            fightLog.FighterB = monsters[i + 1].Name;
+
+            // --------------------------------------------- TODO
+
+            DisplayResults();
+        }
+    }
+
+    void DisplayResults()
+    {
+        string resultMessage = (fightLog.FighterA + " vs " + fightLog.FighterB + " | " + fightLog.Winner + " | Turns: " + fightLog.TotalTurns);
+
+        if (fightLog.Winner == "Draw")
+        {
+            Debug.Log(resultMessage);
+        }
+        else
+        {
+            Debug.Log(resultMessage + " Remaining HP: " + fightLog.WinnerFinalHP);
+        }
     }
 }
