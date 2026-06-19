fd01782dde4091b72f7937409914aba64cac2c76
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-06-18 21:59:57 -0500

    changed the x and y limits of DestroyOutOfBoundsX so that the balls & dog get destroyed when leaving the screen bounds

--- Exercises/PlayFetch/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Exercises/PlayFetch/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -1,25 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
-    private float bottomLimit = -5;
+    private float leftLimit = -25.0f;
+    private float bottomLimit = -5.0f;
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
-        {
-            Destroy(gameObject);
-        } 
-        // Destroy balls if y position is less than bottomLimit
-        else if (transform.position.z < bottomLimit)
+        if (this.transform.position.x < leftLimit)
         {
             Destroy(gameObject);
         }
-
+        // Destroy balls if y position is less than bottomLimit
+        if (this.transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+        }
     }
 }
