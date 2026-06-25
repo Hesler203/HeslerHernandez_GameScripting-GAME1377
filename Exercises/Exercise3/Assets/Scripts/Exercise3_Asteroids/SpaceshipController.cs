c096a3127b516fd1dfe5b26c9ef9985303e7dbd8
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-06-24 20:56:58 -0500

    implemented thrust movement using the rigidbody add force & thrust input

--- Exercises/Exercise3/Assets/Scripts/Exercise3_Asteroids/SpaceshipController.cs
+++ Exercises/Exercise3/Assets/Scripts/Exercise3_Asteroids/SpaceshipController.cs
@@ -1,25 +1,24 @@
 /*
  * Assignment: AsteroidsGame - SpaceshipController Script - PART 1 & 2
- * 
+ *
  * Objective:
  * Implement a player controller for a spaceship in an Asteroids prototype. The player should be able to rotate the ship,
- * move forward, wrap around the screen, and shoot bullets. 
- * 
+ * move forward, wrap around the screen, and shoot bullets.
+ *
  * Requirements:
  * PART 1: Player Movement
  * 1. The player should be able to rotate the ship left and right using A/D keys from an input axis.
- *      This movement should be done with Transform based movement. 
+ *      This movement should be done with Transform based movement.
  * 2. The player should be able to thrust forward using only the W key from an input axis
- *      This movement should be done with physics applied to a RigidBody2D. 
+ *      This movement should be done with physics applied to a RigidBody2D.
  * 3. The player should be able to wrap around the screen when they go off one edge and come back on the other side.
- * 4. The player should be able to teleport to a random location on the screen using left shift in an input button. You 
- *      do not need to check if there is an asteroid there. 
+ * 4. The player should be able to teleport to a random location on the screen using left shift in an input button. You
+ *      do not need to check if there is an asteroid there.
  *      Hint: For determining the random location, you can use the ScreenBounds class (see ScreenWrap.cs for how to use)
- *      
+ *
  * PART 2: Shooting
  * 1. The player should be able to shoot bullets using the space key in an input button
  *      Bullets should only go in the direction the ship is facing and bullet speed should be controlled by the Bullet.cs
- 
  */
 
 using UnityEngine;
@@ -61,7 +60,10 @@ public class AsteroidsPlayerController : MonoBehaviour
 
     private void HandleThrust()
     {
-        
+        if (thrustInput > .01f) // prevents backward movement plus ensures deadzone
+        {
+            rb.AddForce(transform.up.normalized * thrustInput * thrustForce);
+        }
     }
 
     private void HandleFire()
@@ -89,6 +91,8 @@ public class AsteroidsPlayerController : MonoBehaviour
 
     private void TeleportToRandomLocation()
     {
+        float randomXInBounds = Random.Range(ScreenBounds.ScreenLeft, ScreenBounds.ScreenRight);
 
+        Vector3 teleportPosition = new Vector3(randomXInBounds, randomYInBounds);
     }
 }
