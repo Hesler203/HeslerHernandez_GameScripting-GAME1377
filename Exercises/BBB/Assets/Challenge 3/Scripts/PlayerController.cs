e8b6144d2d66e6a107fa292e80f24608dfead853
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-07-01 21:22:54 -0500

    Added a bounce mechanic to the playerController & top bounds limit for the player height
    using a Top bounds to limit the player height allowed by clamping the player's y-position to this top bound & only allowing float below that limit
    
    added an Impulse force to allow the player to bounce upward when colliding with the ground

--- Exercises/BBB/Assets/Challenge 3/Scripts/PlayerController.cs
+++ Exercises/BBB/Assets/Challenge 3/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction floatAction;
+    private const float TOP_BOUND = 15f;
 
     private Rigidbody playerRb;
     [SerializeField] private float floatForce;
     [SerializeField] private float initialForce = 5f;
+    [SerializeField] private float bounceForce = 15f;
     [SerializeField] private float gravityModifier = 1.5f;
 
     [SerializeField] private ParticleSystem explosionParticle;
@@ -29,14 +31,27 @@ public class PlayerController : MonoBehaviour
         HandleFloat();
     }
 
+    /// <summary>
+    /// Adds an upward acceleration to the player while they are pressing the spacebar & game is active,
+    /// Player will reach a terminal velocity determined by the Rigidbody's linear drag,
+    /// The player's max height is clamped to a set Top Bound
+    /// </summary>
     private void HandleFloat()
     {
+        // Clamp the player's max height
+        if (transform.position.y < TOP_BOUND)
+        {
             // While space is pressed and the game is active, float up
             if (floatAction.IsPressed() && !GameManager.IsGameOver)
             {
                 playerRb.AddForce(Vector3.up * floatForce, ForceMode.Acceleration);
             }
         }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, TOP_BOUND);
+        }
+    }
 
     void OnCollisionEnter(Collision other)
     {
@@ -51,6 +66,7 @@ public class PlayerController : MonoBehaviour
         // if player collides with ground, bounce
         if (other.gameObject.CompareTag("Ground"))
         {
+            playerRb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
         }
     }
 
