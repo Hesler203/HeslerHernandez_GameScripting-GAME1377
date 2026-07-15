ab7ba7f4724f444d5ef1a761bbe5acfade62cee6
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-07-14 21:06:50 -0500

    Creating a new project folder for Challenge 4 - Soccer & imported the challenge assets

---
+++ Exercises/Soccer/Assets/Challenge 4/Scripts/RotateCameraX.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class RotateCameraX : MonoBehaviour
+{
+    private float speed = 200;
+    public GameObject player;
+    private InputAction controls;
+
+    void Awake()
+    {
+        controls = new InputAction();
+    }
+
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = controls.ReadValue<Vector2>().x;
+        transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
+
+        transform.position = player.transform.position; // Move focal point with player
+
+    }
+}
