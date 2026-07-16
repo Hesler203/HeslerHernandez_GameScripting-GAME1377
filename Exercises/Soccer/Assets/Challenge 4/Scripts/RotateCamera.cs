eeeb968dafc500f0f08d4ed8a7df8de48b3541d7
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-07-16 00:02:55 -0500

    added references to the player & input action asset used to manage inputs
    added the look action to the input settings to use the mouse delta x for horizontal camera movement
    
    initialized the look action & assigned its read value to a horizontal input variable that is used to rotate the camera while keeping the camera position locked to the player transform position

---
+++ Exercises/Soccer/Assets/Challenge 4/Scripts/RotateCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RotateCamera : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject player;
+    [SerializeField] private InputActionAsset inputActions;
+
+    [Header("Settings")]
+    [SerializeField] private float lookSpeed = 10f;
+    private InputAction lookAction;
+
+    void Awake()
+    {
+        lookAction = inputActions.FindAction("Look", true);
+    }
+
+    void OnEnable()
+    {
+        lookAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        lookAction.Disable();
+    }
+
+    void Update()
+    {
+        float horizontalInput = lookAction.ReadValue<Vector2>().x;
+        transform.Rotate(Vector3.up * horizontalInput * lookSpeed * Time.deltaTime);
+
+        transform.position = player.transform.position;
+    }
+}
