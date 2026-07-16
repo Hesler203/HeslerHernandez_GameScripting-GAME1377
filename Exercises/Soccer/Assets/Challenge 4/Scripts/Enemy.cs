6cb2e0238a79ca6a91a2a2f67f2a23776702efc0
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-07-15 23:39:33 -0500

    Added reference to spawn manager to take advantage of its references to the player & player goal objects which the spawned enemy instances wouldn't have
    added a public method SetSpawnerRef() that would pass in the reference to the spawn manager for the enemy script to use for determining direction vectors towards the player & player goal
    
    added method GetDirectionToTarget() which returns the vector3 direction towards a target transform passed in via the spawn manager's public references to the player & player goal
    
    the target directions are calculated in update
    
    applied the addForce() to the enemy rigidbody to allow movement towards the player goal direction, done in Fixed Update

--- Exercises/Soccer/Assets/Challenge 4/Scripts/Enemy.cs
+++ Exercises/Soccer/Assets/Challenge 4/Scripts/Enemy.cs
@@ -4,13 +4,30 @@ public class Enemy : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] public float speed;
+    private Vector3 playerGoalDirection;
+    public Vector3 playerDirection;
 
     private Rigidbody enemyRb;
+    private SpawnManager spawnManager;
 
+    /// <summary>
+    /// Sets the reference to the spawnManager script, called during spawning.
+    /// </summary>
+    /// <param name="spawnManager">Reference to the spawnManager script.</param>
+    public void SetSpawnerRef(SpawnManager spawnManager)
+    {
+        this.spawnManager = spawnManager;
+    }
 
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        playerGoalDirection = GetDirectionToTarget(spawnManager.playerGoal);
+        playerDirection = GetDirectionToTarget(spawnManager.player.transform);
     }
 
     void FixedUpdate()
@@ -19,11 +36,21 @@ public class Enemy : MonoBehaviour
     }
 
     /// <summary>
+    /// Sets the direction vector from the enemy to the target, normalized & with zero vertical value.
+    /// </summary>
+    private Vector3 GetDirectionToTarget(Transform target)
+    {
+        Vector3 targetDirection = (target.position - transform.position).normalized;
+        return new Vector3(targetDirection.x, 0, targetDirection.z);
+    }
+
+    /// <summary>
     /// Applies a constant acceleration toward the player goal while
     /// linear drag caps & smoothens the movement speed.
     /// </summary>
     private void MoveToPlayerGoal()
     {
+        enemyRb.AddForce(playerGoalDirection * speed, ForceMode.Acceleration);
     }
 
     void OnCollisionEnter(Collision other)
