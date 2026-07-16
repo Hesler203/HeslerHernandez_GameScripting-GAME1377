ec8e2ac422d50a1b5ba7d0ecb8d8f6480127ebb4
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-07-15 23:31:54 -0500

    Setting up enemy movement to use rigidbody & detect collision
    gave enemy linear drag of 1 to prevent infinite acceleration
    
    assigned enemy tag to the enemy prefab
    & powerup tag to powerup prefab

---
+++ Exercises/Soccer/Assets/Challenge 4/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] public float speed;
+
+    private Rigidbody enemyRb;
+
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        MoveToPlayerGoal();
+    }
+
+    /// <summary>
+    /// Applies a constant acceleration toward the player goal while
+    /// linear drag caps & smoothens the movement speed.
+    /// </summary>
+    private void MoveToPlayerGoal()
+    {
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+    }
+}
