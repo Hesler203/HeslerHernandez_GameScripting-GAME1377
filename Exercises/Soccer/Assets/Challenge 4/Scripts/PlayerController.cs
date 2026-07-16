173c5b0636d99d0901f7f1376f66a0679733d526
Hesler203 <165882615+Hesler203@users.noreply.github.com>
2026-07-16 00:20:07 -0500

    added references to the focal point for positioning player relative to camera & added a move action to the input actions asset that uses the w & s keys to move the player forward or backwards
    initialized the move action reference
    
    added a depthInput variable to store the read values from the move action input to be used for movement
    
    set the moveDirection vector of the player to be the camera's forward vector & added an acceleration force in this direction modified by the depthInput for forward & backward moving
    
    the acceleration is capped by the linear drag of 1 set in the inspector & occurs past a deadzone amount for input

---
+++ Exercises/Soccer/Assets/Challenge 4/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private Transform focalPoint;
+    [SerializeField] private InputActionAsset inputActions;
+    [SerializeField] private float moveDeadzone = 0.001f;
+    [SerializeField] private float speed = 3;
+    private Rigidbody playerRb;
+    private InputAction moveAction;
+    private float depthInput;
+    private Vector3 moveDirection;
+
+    void Awake()
+    {
+        moveAction = inputActions.FindAction("Move", true);
+        boostAction = inputActions.FindAction("Boost", true);
+    }
+
+    void OnEnable()
+    {
+        moveAction.Enable();
+        boostAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        moveAction.Disable();
+        boostAction.Disable();
+    }
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        moveDirection = focalPoint.forward;
+        depthInput = moveAction.ReadValue<Vector2>().y;
+    }
+
+    void FixedUpdate()
+    {
+        if (depthInput > moveDeadzone || depthInput < -moveDeadzone)
+        {
+            playerRb.AddForce(moveDirection * depthInput * speed, ForceMode.Acceleration);
+        }
+    }
+}
